6070f39d8633830ad4062af87236aab3572c0ddc
Karl Mabunga <karlmabunga@yahoo.com>
2021-03-02 17:51:47 -0800

    Added state and use effect to grab json placeholder data when page loads and console logging it

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,20 @@
-import React, {useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useCombobox } from 'downshift';
 import { Input } from 'antd';
 
 const App = () => {
+  const [inputItems, setInputItems] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [singleUser, setSingleUser] = useState('');
 
 
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((res) => res.json())
+      .then((data) => setUsers(data))
+  }, [])
+
+  console.log(users);
   return (
     <div className='App'>
       Hello World
